5c287f79dd918aba82e4cd121579deb23a6f119e
Sean Knox <sknox@deis.com>
2017-02-18 15:46:52 -0800

    pkg example

---
+++ chapter11/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/seanknox/golang-book/chapter11/math"
+)
+
+func main() {
+	xs := []float64{1, 2, 3, 4}
+	avg := math.Average(xs)
+	fmt.Println(avg)
+}
